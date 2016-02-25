3a0695c06a76ac52f22e7fe70cd29d9a844c4fdd
Mårten Hansson <hmrten@gmail.com>
2016-02-25 13:38:43 +0100

    add Parking and ParkingSlot db models

---
+++ Garage3/Models/Parking.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Garage3.Models
+{
+    public class Parking
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ParkingSlotId { get; set; }
+
+        [Required]
+        public int VehicleId { get; set; }
+
+        [Required]
+        public DateTime DateIn { get; set; }
+
+        public DateTime? DateOut { get; set; }
+
+        [ForeignKey("VehicleId")]
+        public virtual Vehicle Vehicle { get; set; }
+    }
+}
---
+++ Garage3/Models/ParkingSlot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Garage3.Models
+{
+    public class ParkingSlot
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int? VehicleId { get; set; }
+
+        [ForeignKey("VehicleId")]
+        public virtual Vehicle Vehicle { get; set; }
+    }
+}
